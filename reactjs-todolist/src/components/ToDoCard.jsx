7e35246ce2068329e5269f85ec877b4084c14317
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2024-12-21 15:11:19 +0530

    passed props to ToDoCard fn & declared JS DESTRUCTURING

--- reactjs-todolist/src/components/ToDoCard.jsx
+++ reactjs-todolist/src/components/ToDoCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function ToDoCard() {
+// receive children content via props or properties of the component
+
+export default function ToDoCard(props) {
+  // below is the syntax of JAVASCRIPT DESTRUCTURING
+  // It is a shorthand syntax to extract "children" property from the "props" object, later assigning it to "children" variable.
+  // Same as saying... const children = props.children
+  const {children} = props  
   return (
-    <div>
-      TodoCard
-    </div>
+    // the parent element must have a unique key, because the index of each element in an array is always unique
+    <li className='todoItem'>
+      {children /* calling this children will render <p>{todo}</p> */}  
+      <div className='actionsContainer'>
+        <i className="fa-regular fa-pen-to-square"></i>
+        <i className="fa-solid fa-trash-can"></i>
+      </div>
+    </li>
   )
 }
