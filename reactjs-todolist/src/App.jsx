fe59b8d670e772431f8ce3815d605d4ba82f1f6c
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2024-12-23 12:58:15 +0530

    added useEffect for localStorage

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ToDoInput from "./components/ToDoInput"
 import ToDoList from "./components/ToDoList"
 
@@ -8,49 +8,69 @@ import ToDoList from "./components/ToDoList"
 // App is rendered in the main.jsx component!
 function App() {
 
-  // now we have stateful variable to interact with!
-  const [todos, setTodos] = useState([])
-  // to hold the current value declared by the user in the input field...
-  const [todoValue, setTodoValue] = useState('')
+    // now we have stateful variable to interact with!
+    const [todos, setTodos] = useState([])
+    // to hold the current value declared by the user in the input field...
+    const [todoValue, setTodoValue] = useState('')
 
-  function handleAddTodos(newTodo) {
-    /* 
-      1. This function creates a new array named "newTodoList" using the spread operator "...". This operator takes all the existing values from the "todos" state array and spreads them into the new array.
+    function persistData(newList) {
+        localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+    }
 
-      2. Add "newTodo" string to the end of the "newTodoList" array.
-
-      3. Update the State: Call the "setTodos" function provided by the "useState" hook, to update the "todos" state with the "newTodoList" array. This triggers a re-render of the "App" component and its children (including "ToDoList").
-    */
-    const newTodoList = [...todos, newTodo]
-    setTodos(newTodoList)
-  }
-
-  function handleDeleteTodo(index) {
-    // filter method creates a new array containing only the elements that pass a certain condition (provided as a callback function). The callback function is executed for each element in the todos array
-    const newTodoList = todos.filter((todo, todoIndex) => {
-      return todoIndex !== index
-    })
-    setTodos(newTodoList)
-  }
-
-  function handleEditTodo(index) {
-    const valueToBeEdited = todos[index]
-    setTodoValue(valueToBeEdited)
-    handleDeleteTodo(index)
-  }
-
-  return (
-    // what we return from a function is known as JSX!
-    // JS goes insides the CURLY BRACES {} in JSX
+    function handleAddTodos(newTodo) {
+        /* 
+          1. This function creates a new array named "newTodoList" using the spread operator "...". This operator takes all the existing values from the "todos" state array and spreads them into the new array.
     
-    // We can render our sub-components in <main> tag, as if they are regular HTML Tags, the only difference is that, they are Capitalized!\
-    // below is an EMPTY FRAGMENT (Empty Div)
-    <>
-      <ToDoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} /* This attribute will be received by props passed as an argument in ToDoInput functional component */ />
-      <ToDoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} /* passing attributes to ToDoList Component Tag */ /> 
+          2. Add "newTodo" string to the end of the "newTodoList" array.
+    
+          3. Update the State: Call the "setTodos" function provided by the "useState" hook, to update the "todos" state with the "newTodoList" array. This triggers a re-render of the "App" component and its children (including "ToDoList").
+        */
+        const newTodoList = [...todos, newTodo]
+        persistData(newTodoList)
+        setTodos(newTodoList)
+    }
 
-    </>
-  )
+    function handleDeleteTodo(index) {
+        // filter method creates a new array containing only the elements that pass a certain condition (provided as a callback function). The callback function is executed for each element in the todos array
+        const newTodoList = todos.filter((todo, todoIndex) => {
+            return todoIndex !== index
+        })
+        persistData(newTodoList)
+        setTodos(newTodoList)
+    }
+
+    function handleEditTodo(index) {
+        const valueToBeEdited = todos[index]
+        setTodoValue(valueToBeEdited)
+        handleDeleteTodo(index)
+    }
+
+    useEffect(() => {
+        if (!localStorage) {
+            return
+        }
+
+        let localTodos = localStorage.getItem('todos')
+        if (!localTodos) {
+            return
+        }
+
+        localTodos = JSON.parse(localTodos).todos
+        setTodos(localTodos)
+    }, [])  // empty array means that this effect will only run once, when the page reloads
+
+    return (
+        // what we return from a function is known as JSX!
+        // JS goes insides the CURLY BRACES {} in JSX
+
+        // We can render our sub-components in <main> tag, as if they are regular HTML Tags, the only difference is that, they are Capitalized!\
+        // below is an EMPTY FRAGMENT (Empty Div)
+        <>
+            <ToDoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} /* This attribute will be received by props passed as an argument in ToDoInput functional component */ />
+            <ToDoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} /* passing attributes to ToDoList Component Tag */ />
+
+        </>
+    )
 }
 
 export default App
