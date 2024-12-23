f90724f1a87002a165ae0efaa5a4b5524db09e1e
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2024-12-23 12:43:14 +0530

    passed props for handleEditTodo()

--- reactjs-todolist/src/components/ToDoCard.jsx
+++ reactjs-todolist/src/components/ToDoCard.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 
 // receive children content via props or properties of the component
-
 export default function ToDoCard(props) {
-  // below is the syntax of JAVASCRIPT DESTRUCTURING
-  // It is a shorthand syntax to extract "children" property from the "props" object, later assigning it to "children" variable.
-  // Same as saying... const children = props.children
-  const { children, handleDeleteTodo, index } = props
-  return (
-    // the parent element must have a unique key, because the index of each element in an array is always unique
-    <li className='todoItem'>
-      {children /* calling this children will render <p>{todo}</p> */}
-      <div className='actionsContainer'>
-        <button>
-          <i className="fa-regular fa-pen-to-square"></i>
-        </button>
-        <button onClick={() => {
-          handleDeleteTodo(index)
-        }}>
-          <i className="fa-solid fa-trash-can"></i>
-        </button>
+    // below is the syntax of JAVASCRIPT DESTRUCTURING
+    // It is a shorthand syntax to extract "children" property from the "props" object, later assigning it to "children" variable.
+    // Same as saying... const children = props.children
+    const { children, handleDeleteTodo, index, handleEditTodo } = props
+    return (
+        <li className='todoItem'>
+            {children /* calling this children will render <p>{todo}</p> */}
+            <div className='actionsContainer'>
 
-      </div>
-    </li>
-  )
+                {/* Edit button */}
+                <button onClick={() => { handleEditTodo(index) }}>
+                    <i className="fa-regular fa-pen-to-square"></i>
+                </button>
+
+                {/* Delete button */}
+                <button onClick={() => { handleDeleteTodo(index) }}>
+                    <i className="fa-solid fa-trash-can"></i>
+                </button>
+
+            </div>
+        </li>
+    )
 }
--- reactjs-todolist/src/components/ToDoList.jsx
+++ reactjs-todolist/src/components/ToDoList.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import ToDoCard from './ToDoCard'
 
 export default function ToDoList(props) {
-  const { todos } = props
-  return (
-    <ul className='main'>
-      {/* To render the above array values, the todos... we'll map our content */
-        todos.map((todo, todoIndex) => {
-          return (
-            <ToDoCard key={todoIndex} {...props} index={todoIndex}>
-              {/* 
-                To render out children contents within the Parent component (here ToDoCard is a component), we receive childrens via PROPS (in ToDoCard)
-                
-                ToDoCard is a child component, and the parent ToDoList passes a value <p>{todo}</p> as a children to the ToDoCard component.
-              */}
-              <p>{todo}</p>
-            </ToDoCard>
-          )
-        })}
+    const { todos } = props
+    return (
+        <ul className='main'>
+            {/* To render the above array values, the todos... we'll map our content */
+                todos.map((todo, todoIndex) => {
+                    return (
+                        <ToDoCard key={todoIndex} {...props} index={todoIndex}>
+                            {/* 
+                                To render out children contents within the Parent component (here ToDoCard is a component), we receive childrens via PROPS (in ToDoCard)
+                                
+                                ToDoCard is a child component, and the parent ToDoList passes a value <p>{todo}</p> as a children to the ToDoCard component.
+                            */}
+                            <p>{todo}</p>
+                        </ToDoCard>
+                    )
+                })}
 
-    </ul>
-  )
+        </ul>
+    )
 }
