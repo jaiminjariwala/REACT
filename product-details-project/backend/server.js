8e4acb7fb1ff47a3328315fe9517719f7077f74a
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-03-16 21:59:54 +0530

    created products route

--- product-details-project/backend/server.js
+++ product-details-project/backend/server.js
@@ -1,23 +1,31 @@
 // this is gonna be the entry point for our API
 
-// Before ESM, JavaScript mainly used CommonJS (CJS) for modules, but ESM provides a more efficient and standardized way to handle modularity, especially for browsers and modern Node.js applications.
 // import is an ESM syntax, for that, use "type" : "module" in 'package.json'
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDb } from './config/db.js';
+import Product from './models/product.model.js'
 
-// dotenv.config(): This line tells dotenv to read your .env file (e.g., MONGO_URI=mongodb://localhost:27017/mydb) and make those variables available in process.env. For example, after this runs, process.env.MONGO_URI holds your MongoDB connection string.
 dotenv.config();
 const app = express();  // express() is a function that creates an Express application. The app variable is now an instance of that application, which you’ll use to define routes and start the server.
 
-
-// Routes are how your server responds to client requests. This is where you define the API’s behavior.
-
-
-
 // let's create a "products" route
-app.get("/products", (req, res) => {
-    // this will give us all the products, that we'll have in our database
+app.post("/products", async (req, res) => {
+    const product = req.body;    // user will send this data
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newProduct = new Product
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.log("Error in Create Product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
 });
 
 
@@ -48,5 +56,3 @@ app.listen(5001, () => {
     such that in terminal we just have to say "npm run dev" instead of 
     "node backend/server.js"
 */
-
-
