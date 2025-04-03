a8dacd9f469eb06c4588ed930edd094d27d3cd41
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-04-03 17:34:38 +0530

    began learning and developing new project

---
+++ movie-project/src/App.jsx
@@ -0,0 +1,13 @@
+// a component is really just any function in JS that return some kind of JSX code.
+import './App.css';
+
+function App() {
+  return (
+    // should only have one parent element
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
