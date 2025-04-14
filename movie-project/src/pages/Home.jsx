be02a8cea5f208cdef47ff0385cd29bf6c05ce2a
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-04-14 17:06:58 +0530

    created Favorites and Home page

--- movie-project/src/pages/Home.jsx
+++ movie-project/src/pages/Home.jsx
@@ -1,15 +1,25 @@
+// State is something where once it's updated, the component will change and re-render itself to show the new state.
+// When a state change occurs, the entire components is re-ran or re-rendered.
+// When we refresh the page, then even the state will reset.
+
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+import "../css/Home.css"
 
 function Home() {
+
+    const [searchQuery, setSearchQuery] = useState();
+
     const movies = [
         { id: 1, title: "John Cooper", release_date: "2020" },
         { id: 2, title: "Terminator", release_date: "1999" },
         { id: 3, title: "The Matrix", release_date: "2014" },
     ];
 
-    const handleSearch = () => {
-
-    }
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+    };
 
     return (
         <div className="home">
@@ -17,14 +27,17 @@ function Home() {
                 <input
                     type="text"
                     placeholder="Search for movies..." className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
 
                 <button type="submit" className="search-button">Search</button>
             </form>
 
             <div className="movies-grid">
-                {movies.map(
-                    (movie) => <MovieCard movie={movie} key={movie.id} />
+                {movies.map((movie) =>
+                    movie.title.toLowerCase().startsWith(searchQuery) &&
+                    (<MovieCard movie={movie} key={movie.id} />)
                 )}
             </div>
         </div>
